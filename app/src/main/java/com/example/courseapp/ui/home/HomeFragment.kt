65cb704d872c016ef4ef23b39116b6160101e9f4
giorgiodishvili <odishvili.giorgi@gmail.com>
2021-07-02 07:20:42 +0400

    first commit

--- app/src/main/java/com/example/courseapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/courseapp/ui/home/HomeFragment.kt
@@ -9,37 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.courseapp.R
+import com.example.courseapp.base.BaseFragment
 import com.example.courseapp.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-
-    private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+    override fun start(inflater: LayoutInflater, container: ViewGroup?) {
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
